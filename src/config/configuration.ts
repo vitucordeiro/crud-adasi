0b5a251e69e735202f36a37217225ae19659e9e4
Victor Cordeiro <victorcordeiro.contact@gmail.com>
2025-05-30 16:51:54 -0300

    CRUD-ADASI - Teste técnico

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,8 +1,8 @@
 export default () => ({
-  port: parseInt(process.env.PORT || '3000', 10),
+  port: process.env.PORT || '3000',
   database: {
     host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432', 10),
+    port: process.env.DB_PORT || '5432',
     username: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || 'postgres',
     database: process.env.DB_DATABASE || 'adasi_db',
